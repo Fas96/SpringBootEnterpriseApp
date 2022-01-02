131c84b429b8c5d795ede2873a9f2d1d526248a1
Fas Bhim <fasbhim@Fass-MacBook-Pro.local>
2022-01-02 13:17:02 +0900

    spring boot actuators and hibernate

---
+++ src/test/java/com/example/enterprisespringbootapp/EnterpriseSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterprisespringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
