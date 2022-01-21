824534c848f9dfc7d9763b680684936fcee8f00e
Fas96 <firibu.anyass@gmail.com>
2022-01-22 02:22:44 +0900

    security handler

--- src/test/java/com/example/enterprisespringbootapp/EnterpriseSpringBootAppApplicationTests.java
+++ src/test/java/com/example/enterprisespringbootapp/EnterpriseSpringBootAppApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.enterprisespringbootapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@org.springframework.boot.test.context.SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@RunWith(SpringRunner.class)
 class EnterpriseSpringBootAppApplicationTests {
 
     @Test
