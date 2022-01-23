717429f20ce4359c2d4fb3aabe62ae87d6162361
Fas96 <firibu.anyass@gmail.com>
2022-01-23 23:01:47 +0900

    security handler

--- src/test/java/com/example/enterprisespringbootapp/EnterpriseSpringBootAppApplicationTests.java
+++ src/test/java/com/example/enterprisespringbootapp/EnterpriseSpringBootAppApplicationTests.java
@@ -3,29 +3,41 @@ package com.example.enterprisespringbootapp;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 //@org.springframework.boot.test.context.SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@org.springframework.boot.test.context.SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@org.springframework.boot.test.context.SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @RunWith(SpringRunner.class)
 @IfProfileValue(name = "",value = "yes")
+@AutoConfigureMockMvc
 class EnterpriseSpringBootAppApplicationTests {
 
-    @LocalServerPort
-    private int port;
+//    @LocalServerPort
+//    private int port;
+//    @Autowired
+//    private TestRestTemplate testRestTemplate;
+
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
     }
 
 
-    public void getCarTest(){
+    @Test
+    public void getCarTest() throws Exception {
+
+        this.mockMvc.perform(get("/cars")).andDo(System.out::println);
+
 
     }
 
