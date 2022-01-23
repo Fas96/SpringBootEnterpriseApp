e6187174fb8345d23705155752dcdf90204e4db6
Fas96 <firibu.anyass@gmail.com>
2022-01-23 22:42:24 +0900

    security handler

--- src/test/java/com/example/enterprisespringbootapp/EnterpriseSpringBootAppApplicationTests.java
+++ src/test/java/com/example/enterprisespringbootapp/EnterpriseSpringBootAppApplicationTests.java
@@ -2,15 +2,31 @@ package com.example.enterprisespringbootapp;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@org.springframework.boot.test.context.SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//@org.springframework.boot.test.context.SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@org.springframework.boot.test.context.SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
+@IfProfileValue(name = "",value = "yes")
 class EnterpriseSpringBootAppApplicationTests {
+
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+
+    public void getCarTest(){
+
     }
 
 }
