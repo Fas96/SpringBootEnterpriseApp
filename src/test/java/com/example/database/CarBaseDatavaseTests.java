193a73db870ad15a80050e403cda9e39fb75f822
Fas96 <firibu.anyass@gmail.com>
2022-01-30 23:24:31 +0900

    clean code

--- src/test/java/com/example/database/CarBaseDatavaseTests.java
+++ src/test/java/com/example/database/CarBaseDatavaseTests.java
@@ -1,8 +1,9 @@
 package com.example.database;
 
 import com.apple.eawt.Application;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.example.enterprisespringbootapp.CarwasherConfiguration;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -15,8 +16,12 @@ import java.util.List;
 import java.util.Map;
 
 @SpringBootTest(classes = Application.class)
-@RunWith(SpringRunner.class)
 public class CarBaseDatavaseTests {
+    @Autowired
+    private CarwasherConfiguration  carwash=new CarwasherConfiguration();
 
-
+    @Test
+    public void getProperties(){
+        carwash.init();
+    }
 }
