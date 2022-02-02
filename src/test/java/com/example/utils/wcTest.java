de5ab0d77e787cd23e7ca925ccaf259023299a0b
Fas96 <firibu.anyass@gmail.com>
2022-02-02 23:10:52 +0900

    clean code

--- src/test/java/com/example/utils/wcTest.java
+++ src/test/java/com/example/utils/wcTest.java
@@ -1,15 +1,28 @@
 package com.example.utils;
 
+
+
+import com.apple.eawt.Application;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.LogManager;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.springframework.boot.test.context.SpringBootTest;
+@Slf4j
 class wcTest {
 
     @Test
     public void one_valid(){
-        Assert.assertSame(1,1);
+
+        Assert.assertSame(1, 1);
     }
 
+    @Test
+    public void  LogAddAppender() {
+        Logger logger = (Logger) LogManager.getLogger("MyLogger");
+
+        logger.info("hello");
+    }
 }
