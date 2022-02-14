bc2c198cd627f1d279a3ab44ebabe7eb2d5ff466
Fas96 <firibu.anyass@gmail.com>
2022-02-14 23:53:36 +0900

    concurrency

---
+++ src/test/java/com/example/threads/MainThreadsTest.java
@@ -0,0 +1,25 @@
+package com.example.threads;
+
+import junit.framework.TestCase;
+import org.apache.logging.log4j.ThreadContext;
+import org.junit.Assert;
+
+public class MainThreadsTest extends TestCase {
+
+    public void test_threads(){
+
+        Assert.assertTrue(2==2);
+        Assert.assertEquals(nextUrlOrNull(),"fas");
+        }
+
+        static  class urlGenerator{
+        public static String next(){
+            return "fas";
+        }
+        }
+    public synchronized static String nextUrlOrNull() {
+        String url =  urlGenerator.next();
+        Thread.yield(); // inserted for testing. updateHasNext();
+        return url;
+    }
+}
